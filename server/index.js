1c1129303cfa08ef2970f07e7e4e0503f4d354c9
Srushtiraj Bhosale <srushtiraj777@gmail.com>
2023-06-20 08:44:02 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,30 +1,29 @@
-// sXrLux0IYLlundYY
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const UserModel = require("./models/Users");
 const cors = require("cors");                  //helps to connect this api with react(frontend);
+require("dotenv").config();
 
+// Middleware
 app.use(express.json());     //helps to parse data requested from body
 app.use(cors());
 
 mongoose.connect(
-    "mongodb+srv://srushtiraj777:sXrLux0IYLlundYY@cluster0.jo8xrxy.mongodb.net/MernPractice"
-);
+    `mongodb://${process.env.DB_USER}:${process.env.DB_PW}@ac-zzcbs4v-shard-00-00.3dx29m9.mongodb.net:27017,ac-zzcbs4v-shard-00-01.3dx29m9.mongodb.net:27017,ac-zzcbs4v-shard-00-02.3dx29m9.mongodb.net:27017/Mern-Basics?ssl=true&replicaSet=atlas-nzrpt2-shard-0&authSource=admin&retryWrites=true&w=majority`
+).then(() => console.log("Connection Suceessful"));
 
-app.get("/getUsers", (req, res) => {
-    UserModel.find().then((result) => {
-        res.json(result)
-    })
+app.get("/getUsers", async(req, res) => {
+    const userData = await UserModel.find();
+    res.status(201).json(userData)
 })
 
-app.post("/createUser", (req, res) => {
-    const user = req.body;
-    const newUser = new UserModel(user);
-    newUser.save();
+app.post("/createUser", async(req, res) => {
+    const userData = new UserModel(req.body);
+    const user = await userData.save();
+    res.status(201).json(user);
 })
 
 app.listen(5000, () => {
-    console.log("Server Runs Perfectly");
+    console.log("Server Running Perfectly");
 })
